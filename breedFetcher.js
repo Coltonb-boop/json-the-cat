b76d5314f4c459c1d6cb40aeff7b654fde776c68
Coltonb-boop <coltonbitz@gmail.com>
2022-08-04 14:54:25 -0600

    figured out why my code was giving me issues. I needed to parse the data in body BEFORE trying to use it in if statements and such

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,8 +4,8 @@ const input = process.argv.slice(2);
 
 const fetch = (breed) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, status, body) => {
-    if (body === []) {
-      const data = JSON.parse(body);
+    const data = JSON.parse(body);
+    if (data[0]) {
       console.log(data[0].description);
     } else {
       console.log(`Sorry! Couldn't find the ${breed} of cat.`);
