ac8c7d1c343bdcc384e9a18983dcd22c08f2b1e7
Coltonb-boop <coltonbitz@gmail.com>
2022-08-04 14:10:56 -0600

    passing first edge case

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,9 +3,13 @@ const request = require('request');
 const input = process.argv.slice(2);
 
 const fetch = (breed) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, status, body) => {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, status, body) => {
+    if (body === []) {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    } else {
+      console.log(`Sorry! Couldn't find the ${breed} of cat.`);
+    }
   });
 };
 
