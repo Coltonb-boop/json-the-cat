30ac88bd94f33457e17cdc35adc88c9f9f4126a9
Coltonb-boop <coltonbitz@gmail.com>
2022-08-04 15:25:02 -0600

    cat searcher is now modular

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,15 +2,11 @@
 const request = require('request');
 const input = process.argv.slice(2);
 
-const fetch = (breed) => {
+const fetchBreedDescription = (breed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, status, body) => {
     const data = JSON.parse(body);
-    if (data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log(`Sorry! Couldn't find the ${breed} of cat.`);
-    }
+    callback(err, data[0].description);
   });
 };
 
-fetch(input);
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+`use strict`;
+let input = process.argv.slice(2);
+const { fetchBreedDescription } = require('./breedFetcher');
+
+fetchBreedDescription(input, (error, desc) => {
+  if (error) {
+    console.error('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+})
